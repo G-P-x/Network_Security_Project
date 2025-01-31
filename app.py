ae65adf363267dcaed23f303ddb2e1ade42fba80
G-P-x <paugiovanni68@yahoo.it>
2025-01-31 19:23:33 +0100

    REMOVED IN THE SERVERS THE REFERENCES TO EACH OTHERS.

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import time
 app = Flask(__name__)
 db = db_interface()
 
-app_2 = Flask(__name__) # by default one server can only listen to one port
-
-
 @app.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -25,20 +22,6 @@ def new_user():
     if db.write(**data):
         return 'Data has been saved!'
     return 'Data has not been saved!'
-
-@app_2.post('/update/database')
-def new_user():
-    data = request.get_json()
-    if db.write(**data):
-        return 'Data has been saved!'
-    return 'Data has not been saved!'
-
-def run_server(**kwargs):
-    application = kwargs['application']
-    assert isinstance(application, Flask)
-    port_int = kwargs['port']
-    assert isinstance(port_int, int)
-    application.run(port=port_int, debug=True, use_reloader=False)
 
 if __name__ == '__main__':
     # thread_1 = Thread(target=run_server, kwargs={'application':app, 'port': 5000, })
--- app_2.py
+++ app_2.py
@@ -3,28 +3,14 @@ from database.database_interface import db_interface
 from threading import Thread
 import time
 
-app = Flask(__name__)
 db = db_interface()
-
 app_2 = Flask(__name__) # by default one server can only listen to one port
 
 
-@app.route('/')
+@app_2.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.get('/message/<sms>')
-def return_message(sms: str):
-    print(sms)
-    new = input(': ')
-    return f'<p style="color: red;">{new}</p>'
-
-@app.post('/update/database')
-def new_user():
-    data = request.get_json()
-    if db.write(**data):
-        return 'Data has been saved!'
-    return 'Data has not been saved!'
 
 @app_2.post('/update/database')
 def new_user():
@@ -32,13 +18,6 @@ def new_user():
     if db.write(**data):
         return 'Data has been saved!'
     return 'Data has not been saved!'
-
-def run_server(**kwargs):
-    application = kwargs['application']
-    assert isinstance(application, Flask)
-    port_int = kwargs['port']
-    assert isinstance(port_int, int)
-    application.run(port=port_int, debug=True, use_reloader=False)
 
 if __name__ == '__main__':
     # thread_2 = Thread(target=run_server, kwargs={'application':app_2, 'port': 5001,})
